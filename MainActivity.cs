04a05e87875295cb571e2b01439cf62c260e7247
Darren Gorman <saintred-tech@hotmail.com>
2019-08-11 02:10:17 +1200

    buttons

--- MainActivity.cs
+++ MainActivity.cs
@@ -3,12 +3,18 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Media;
+using Environment = System.Environment;
+using System;
 
 namespace LcarsHangman
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : Activity
     {
+        // Sound test  
+        MediaPlayer player;
+        // Sound test
         private Button btnStart;
         private RadioButton Rb1, Rb2, Rb3;
 
@@ -20,27 +26,76 @@ namespace LcarsHangman
             SetContentView(Resource.Layout.activity_main);
 
             btnStart = FindViewById<Button>(Resource.Id.btnStart);
-            Rb1 = FindViewById<RadioButton>(Resource.Id.Rb1);
-            Rb2 = FindViewById<RadioButton>(Resource.Id.Rb2);
-            Rb3 = FindViewById<RadioButton>(Resource.Id.Rb3);
+            RadioButton Rb1 = FindViewById<RadioButton>(Resource.Id.Rb1);
+            RadioButton Rb2 = FindViewById<RadioButton>(Resource.Id.Rb2);
+            RadioButton Rb3 = FindViewById<RadioButton>(Resource.Id.Rb3);
 
-            Rb1.Click += Rb1_Click;
             btnStart.Click += BtnStart_Click;
+            Rb1.Click += Rb1_Click;
+            Rb2.Click += Rb1_Click;
+            Rb3.Click += Rb1_Click;
+
+
+
+
         }
         // link this to start button
-        private void BtnStart_Click(object sender, System.EventArgs e)
+        private void BtnStart_Click(object sender, EventArgs e)
         {
-            throw new System.NotImplementedException();
-        }
+            Button fakebutton = (Button)sender;
+            fakBut(fakebutton.Text);
 
+        }
+        private void fakBut(string FakButswitch)
+        {
+            {
+                switch (FakButswitch)
+                {
+                    case "1":
+                        // do stuff here
+                        break;
+
+                    case "2":
+                        // do stuff here
+                        break;
+
+                    case "3":
+                        // do stuff here
+                        break;
+
+                    case "4":
+                        // do stuff here
+                        break;
+                }
+            }
+        }
         // link this to all the radio buttons
         private void Rb1_Click(object sender, System.EventArgs e)
         {
-            throw new System.NotImplementedException();
+            RadioButton fkRb = new RadioButton;
+            fkRb = (RadioButton)sender;
         }
 
         // make a method for buttons
+        private void fakRadio(string FakRaswitch)
+        {
+            switch (FakRaswitch)
+            {
+                case "1":
+                    // do stuff
+                    break;
 
+                case "2":
+                    // do stuff
+                    break;
+
+                case "3":
+                    // do stuff
+                    break;
+
+
+            }
+        }
 
 
 
